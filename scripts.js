745087817a05cc73a378f8c3268038dd9f90f314
Aaron <aaronlo826@gmail.com>
2022-04-01 14:52:59 -0400

    added html and js starter files

---
+++ scripts.js
@@ -0,0 +1,41 @@
+
+// App object pokedex
+const pokedex = {};
+
+// Initalize preset data
+
+pokedex.ApiUrl = "https://pokeapi.co/api/v2/pokemon/";
+
+fetch(pokedex.ApiUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonResult) {
+    console.log("It worked!", jsonResult);
+  });
+
+
+
+  //  Landing page with heading Pokedex
+
+
+  // An input form that allows users to choose pokemon from generation 1 by their Name from a dropdown list
+  // Once user chooses pokemon from dropdown list, user submits it (eventListener)
+  // Method (getUserQuery) to update varaible (userQuery) based on the user input
+  // Store user input in (userQuery)
+
+  // Make AJAX request to retrieve information on Generation 1 pokemon
+  // Create a method (getPokemon) to grab only the first generation of pokemon (151)
+  // When successful, display results by appending the data to the details div
+  // If API call fails, display an error message
+
+  // based on user input, display the corresponding pokemon with the name or ID
+
+  //  pokemon details displayed will be name, id, sprite, and type.
+
+
+
+
+
+
+
