3ccda8365b75df15c3f407edaca2237892673a0a
Aaron <aaronlo826@gmail.com>
2022-04-08 16:43:58 -0400

    added bunch of styling for header, nav, sections and footer

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-// App object pokedexApp
+// App object pokedexAppApp
 const pokedexApp = {};
 
 // Initalize preset data
+// grabs all gen 1 pokemon
 pokedexApp.getGen1Pokemon = function () {
   const url = "https://pokeapi.co/api/v2/pokemon?limit=151/";
 
@@ -10,11 +11,106 @@ pokedexApp.getGen1Pokemon = function () {
       return results.json();
     })
     .then(function (jsonResults) {
-      console.log("It worked!", jsonResults);
-      pokedexApp.getThePokemon(jsonResults);
+      jsonResults.results.forEach(function (poke) {
+        pokedexApp.pokeInfo(poke);
+
+        // console.log(poke)
+      })
+    });
+};
+// Grabbing all gen 1 pokemon info
+pokedexApp.pokeInfo = function (poke) {
+  const pokeUrl = poke.url;
+  fetch(pokeUrl)
+  .then(results => results.json())
+  .then(function (kantoData) {
+      // pokedexApp.displayPokemon(kantoData);
+      pokedexApp.pokeDropbox(kantoData);
+      // console.log(kantoData);
+    })
+  }
+
+  // Appending pokemon names into dropbox
+  pokedexApp.pokeDropbox = function (kantoData) {
+    const options = document.createElement('option')
+    options.value = kantoData.name
+    options.innerHTML = kantoData.name
+
+    // console.log(options);
+    document.querySelector("#pokeDropbox").appendChild(options);
+    pokedexApp.getUserInput();
+
+  }
+  // function to get user input
+  pokedexApp.getUserInput = function () {
+    document.querySelector("#pokeDropbox").addEventListener("change", function () {
+      const pokemon = this.value;
+      console.log(pokemon)
+      // >>>>>>>>>> BREAKS HERE
+      // pokedexApp.singleGen1Pokemon(pokemon);
+    });
+
+  };
+
+  // grabs single pokemon
+  pokedexApp.singleGen1Pokemon = function (query) {
+  const url = new URL ("https://pokeapi.co/api/v2/pokemon/");
+
+  url.search = new URLSearchParams({
+    q: query,
+  });
+  fetch(url)
+    .then(function (results) {
+      return results.json();
+    })
+    .then(function (jsonResults) {
+      // WE DONT WANT FOR EACH ONLY ONE RESULT
+      jsonResults.results.forEach(function (poke) {
+        pokedexApp.pokeInfo2(poke);
+        console.log(poke)
+      });
+    });
+};
+// Getting only 1 pokemon's information
+pokedexApp.pokeInfo2 = function (poke) {
+  const pokeUrl = poke.url;
+  fetch(pokeUrl)
+    .then((results) => results.json())
+    .then(function (kantoData) {
+      // pokedexApp.displayPokemon(kantoData);
     });
 };
 
+// displaying pokemon details into div
+pokedexApp.displayPokemon = function (kantoData) {
+  const name = document.createElement('h2')
+  name.innerHTML = kantoData.name;
+
+  const type = document.createElement('p')
+  type.innerText = kantoData.types[0].type.name
+
+  // needs to add both types
+
+  console.log(type)
+
+  const images = document.createElement("img");
+  images.src = kantoData.sprites.front_default;
+  images.alt = kantoData.name;
+  // console.log(images);
+  // document.querySelector("#pokemon").appendChild(images);
+
+  const div = document.createElement('div');
+  div.classList.add('pokemonContainer');
+
+  div.appendChild(name);
+  div.appendChild(type);
+  div.appendChild(images);
+
+  document.querySelector("#pokemon").appendChild(div);
+}
+
+
+// Grabbing all gen 1 pokemon information
 pokedexApp.getThePokemon = function (pokemonArray) {
   pokemonArray.results.forEach(function (pokemonObject) {
     console.log(pokemonObject);
@@ -22,22 +118,19 @@ pokedexApp.getThePokemon = function (pokemonArray) {
   });
 };
 
-pokedexApp.getPokeNames = function (pokemonObject) {
-  const pokemon = pokemonObject.name;
-  // console.log(pokemon);
-  const pokeName = document.createElement('p');
-  pokemonObject
-};
+
 
 
 
 // init method to kick things off
 pokedexApp.init = function () {
   console.log("init success");
-  pokedexApp.getGen1Pokemon();
+  pokedexApp.getGen1Pokemon(pokemon);
 }
 
 pokedexApp.init();
+
+//  Landing page with heading pokedexApp
 
 // testing 1232323
 
@@ -69,3 +162,5 @@ pokedexApp.init();
 // then using this fetch the new url of the api
 // this would give us information of the said pokemon
 // append specific data from the pokemon (id name sprite type) into something (ul or li)
+
+// MY COMMENT
